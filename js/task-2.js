bbc4cacfe3baf33cbc7a49103ead3f19b3019e44
JuliaCicer13 <ciceron131313@gmail.com>
2024-01-03 02:23:41 +0200

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -2,7 +2,33 @@ class Storage  {
     
     #items;
 
+    constructor(initialItems) {
+        this.#items = initialItems;
+    }
 
-   
+    getItems() {
+        return this.#items;
+    }
+    addItem(newItem) {
+        this.#items.push(newItem)
+    }
+    removeItem(itemToRemove) {
+        this.#items =
+            this.#items.filter(item => item !== itemToRemove);
+    }
 }
-getItems() { return this.#items; }
+
+
+
+
+
+
+
+
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
