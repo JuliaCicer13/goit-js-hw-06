5cc946b73315a20cc265a91974d457641fbba560
JuliaCicer13 <ciceron131313@gmail.com>
2024-01-03 18:14:26 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,3 +1,31 @@
 class StringBuilder {
-    
+    #value;
+
+    constructor(value) {
+        this.#value = value;
+    }
+
+    getValue() {
+        return this.#value;
+    }
+    padEnd(str) {
+        this.#value += str;
+        return this;
+    }
+    padStart(str) {
+        this.#value += str;
+        return this;
+    }
+    padBoth(str) {
+        this.padStart(str) + this.padEnd(str);
+        return this;
+    }
 }
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
